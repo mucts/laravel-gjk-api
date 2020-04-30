f429bda9dd71072a54b7aed46017b9640fff773a
herry <yuandeng@aliyun.com>
2020-04-30 09:49:58 +0800

    fix bug

--- src/helpers.php
+++ src/helpers.php
@@ -14,9 +14,9 @@ if (!function_exists('gjk_request')) {
      */
     function gjk_request($route, $params)
     {
-        $params = array_merge(["app_id" => config('gjk.app_id'), "user_id" => mt_rand(1000000, 3000000), "c_ver" => config('gjk.secret_key'), "nonce_str" => str_random(mt_rand(6, 32))], $params);
+        $params = array_merge(["app_id" => config('gjk.app_id'), "user_id" => mt_rand(1000000, 3000000), "c_ver" => config('gjk.version'), "nonce_str" => str_random(mt_rand(6, 32))], $params);
         $params = array_merge($params, ["sign" => Sign::makeSign($params, config('gjk.secret_key'))]);
-        $url = trim('gjk.url', '/') . '/' . ltrim($route, '/');
+        $url = trim(config('gjk.url'), '/') . '/' . ltrim($route, '/');
         $response = api_request($url, 'post', $params);
         return $response ? json_decode($response, true) : null;
     }
