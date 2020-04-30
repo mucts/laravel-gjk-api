8cab3fb41b54d95e1d7074a1b36a9cc779de817e
herry <yuandeng@aliyun.com>
2020-04-30 08:14:26 +0800

    init

---
+++ src/Sign.php
@@ -0,0 +1,64 @@
+<?php
+
+
+namespace MuCTS\LaravelGuiJkAPI;
+
+
+class Sign
+{
+    /**
+     * 校验签名
+     *
+     * @param array $param
+     * @return bool
+     */
+    public static function verifySign(array $param)
+    {
+        return self::makeSign($param) == @$param['sign'];
+    }
+
+    /**
+     * 签名
+     * @param array $param
+     * @param string $secretKey
+     * @return string
+     * @author herry.yao<yao.yuandeng@qianka.com>
+     */
+    public static function makeSign(array $param, $secretKey)
+    {
+        self::_dictSort($param);
+        return strtoupper(md5(self::_createLinkString($param) . '&key=' . $secretKey));
+    }
+
+    /**
+     * 字典排序
+     * @param array $param 待排序参数
+     */
+    private static function _dictSort(array &$param)
+    {
+        ksort($param);
+        reset($param);
+    }
+
+    /**
+     * 字符串拼接并去除转义
+     * @param array $param 带拼接数组
+     * @return string
+     */
+    private static function _createLinkString(array $param = [])
+    {
+        $linkString = "";
+        if (is_array($param)) {
+            foreach ($param as $key => $value) {
+                if (is_null($value) || $value === "" || $key === "sign" || $key == "sign_type") continue;
+                $value = is_array($value) ? json_encode($value, JSON_UNESCAPED_UNICODE) : $value;
+                $linkString .= "{$key}={$value}&";
+            }
+        }
+        $linkString = substr($linkString, 0, strlen($linkString) - 1);
+        if (get_magic_quotes_gpc()) {
+            $linkString = stripcslashes($linkString);
+        }
+        return $linkString;
+    }
+}
---
+++ src/helpers.php
@@ -0,0 +1,23 @@
+<?php
+
+use MuCTS\LaravelGuiJkAPI\Sign;
+
+if (!function_exists('gjk_request')) {
+    /**
+     * Gjk API Request
+     *
+     * @param string $route
+     * @param array $params
+     * @return string|array|null
+     * @throws Exception
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    function gjk_request($route, $params)
+    {
+        $params = array_merge(["app_id" => config('gjk.app_id'), "user_id" => mt_rand(1000000, 3000000), "c_ver" => config('gjk.secret_key'), "nonce_str" => str_random(mt_rand(6, 32))], $params);
+        $params = array_merge($params, ["sign" => Sign::makeSign($params, config('gjk.secret_key'))]);
+        $url = trim('gjk.url', '/') . '/' . ltrim($route, '/');
+        $response = api_request($url, 'post', $params);
+        return $response ? json_decode($response, true) : null;
+    }
+}
