d17bc9d7ff2a67c17d020f1f009471aef702186c
herry <yuandeng@aliyun.com>
2020-05-22 16:32:29 +0800

    fix  bug

--- src/Sign.php
+++ src/Sign.php
@@ -39,7 +39,7 @@ trait Sign
      *
      * @param array $param 待排序参数
      */
-    private  function _dictSort(array &$param)
+    private function _dictSort(array &$param)
     {
         ksort($param);
         reset($param);
@@ -50,15 +50,13 @@ trait Sign
      * @param array $param 带拼接数组
      * @return string
      */
-    private  function _createLinkString(array $param = [])
+    private function _createLinkString(array $param = [])
     {
-        $linkString = "";
-        foreach ($param as $key => $value) {
-            if (is_null($value) || $value === "" || $key === "sign" || $key == "sign_type") continue;
-            $value = is_array($value) ? json_encode($value, JSON_UNESCAPED_UNICODE) : $value;
-            $linkString .= "{$key}={$value}&";
-        }
-        $linkString = substr($linkString, 0, strlen($linkString) - 1);
-        return $linkString;
+        return collect($param)->filter(function ($value, $key) {
+            return !is_null($value) && $value !== '' && !Arr::exists(['sign', 'sign_type'], $key);
+        })->map(function ($value, $key) {
+            return sprintf('%s=%s', $key,
+                is_array($value) ? json_encode($value, JSON_UNESCAPED_UNICODE) : $value);
+        })->implode('&');
     }
 }
